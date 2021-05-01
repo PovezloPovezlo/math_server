cefc0fb233f7c96b0687d7193af140e65b1c7c8d
BagaevDanil <82843922+BagaevDanil@users.noreply.github.com>
2021-05-01 22:46:40 +0300

    Реализовал функцию COM_NN_D

--- test/module/NTest.cpp
+++ test/module/NTest.cpp
@@ -8,3 +8,14 @@ using namespace base;
 using namespace module;
 
 // тесты
+TEST(N, COM_NN_D)
+{
+	ULongNumber a("12345");
+	ULongNumber b("56788");
+	ULongNumber c("634");
+	ULongNumber d("12345");
+	EXPECT_EQ(COM_NN_D(a, b), 1);
+	EXPECT_EQ(COM_NN_D(b, c), 2);
+	EXPECT_EQ(COM_NN_D(d, a), 0);
+	EXPECT_EQ(COM_NN_D(c, d), 1);
+}
