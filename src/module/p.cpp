8fd93da30987ca6504567d5f7350b068fb931410
NolikTop <aydansuper12@gmail.com>
2021-05-19 21:47:46 +0300

    feat: Обновил свою аву и некоторые фиксы feat: P-9 и P-8

--- src/module/p.cpp
+++ src/module/p.cpp
@@ -143,7 +143,35 @@ ULongNumber module::DEG_P_N(Polynomial& a) {
  * @return
  */
 RationalFraction module::FAC_P_Q(Polynomial& a) {
-	throw NotImplementedException();
+	auto nod = ULongNumber("1");
+	auto nok = NLongNumber("1");
+
+	for (auto i = a.coefficients.rbegin(); i != a.coefficients.rend(); i++) { //найдём нок и нод
+		auto el = *i;
+
+		auto num = TRANS_Z_N(el->value.numerator);
+		if (i == a.coefficients.rbegin()) nod = num;
+		else nod = GCF_NN_N(nod, num);
+
+		auto denom = el->value.denominator;
+		auto t = LCM_NN_N(nok, denom);
+		nok = NLongNumber::fromLongNumber(t);
+	}
+
+	auto tmp = NLongNumber::fromLongNumber(nod);
+	auto nod_upgr = TRANS_N_Z(tmp); //числитель целый => нам нужен целый аналог НОД
+
+	for (auto i = a.coefficients.rbegin(); i != a.coefficients.rend(); i++) { //выносим НОД и НОК
+		auto el = *i;
+
+		el->value.numerator = DIV_ZZ_Z(el->value.numerator, nod_upgr);
+		auto tmp = DIV_NN_N(nok, el->value.denominator);
+		el->value.denominator = NLongNumber::fromLongNumber(tmp);
+	}
+
+	auto res = RationalFraction(nod_upgr, nok);
+
+	return res;
 }
 
 /**
